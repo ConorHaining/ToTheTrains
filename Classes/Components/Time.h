3ab9daed559a7f1fc6e1781076981a70f1efe57b
Conor Haining <conor.haining@gmail.com>
2018-03-28 13:44:51 +0100

    New Time Component

---
+++ Classes/Components/Time.h
@@ -0,0 +1,18 @@
+//
+// Created by conor on 28/03/18.
+//
+
+#ifndef PROJ_ANDROID_STUDIO_TIME_H
+#define PROJ_ANDROID_STUDIO_TIME_H
+
+
+class Time {
+private:
+    int hour;
+    int minute;
+    int second;
+    float delta;
+};
+
+
+#endif //PROJ_ANDROID_STUDIO_TIME_H
