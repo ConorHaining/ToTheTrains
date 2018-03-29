861ee85c06cd9a2801797a8284ac0e723d8b32aa
Conor Haining <conor.haining@gmail.com>
2018-03-29 11:02:23 +0100

    Remove Closing function

--- Classes/StationScene.cpp
+++ Classes/StationScene.cpp
@@ -29,25 +29,7 @@ bool StationScene::init()
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
 
-    
+
 
     return true;
-}
-
-
-void StationScene::menuCloseCallback(Ref* pSender)
-{
-    //Close the cocos2d-x game scene and quit the application
-    Director::getInstance()->end();
-
-    #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
-#endif
-
-    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() and exit(0) as given above,instead trigger a custom event created in RootViewController.mm as below*/
-
-    //EventCustom customEndEvent("game_scene_close_event");
-    //_eventDispatcher->dispatchEvent(&customEndEvent);
-
-
 }
