48d06d49061c3f581dd859a5bfcb71ba8138b71a
Conor Haining <conor.haining@gmail.com>
2018-03-29 13:57:22 +0100

    Logic to draw station on screen

--- Classes/Systems/StationRenderSystem.cpp
+++ Classes/Systems/StationRenderSystem.cpp
@@ -2,13 +2,26 @@
 // Created by conor on 29/03/18.
 //
 
+#include "cocos2d.h"
+#include <EntityManager.h>
 #include "StationRenderSystem.h"
+#include "../Components/SpriteComponent.h"
+
+USING_NS_CC;
 
 StationRenderSystem::StationRenderSystem(cocos2d::Scene *scene, EntityInterface *station) {
     this->scene = scene;
     this->station = station;
+    log("Assigned Scene & Staton to System");
 }
 
 void StationRenderSystem::drawStation() {
+    SpriteComponent* sprite = (SpriteComponent*)this->station->getComponent(1);
+    log("Got Sprite");
+    auto dirs = Director::getInstance();
+    Size visibleSize = dirs->getVisibleSize();
 
+    sprite->getSprite()->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2));
+
+    this->scene->addChild(sprite->getSprite());
 }
