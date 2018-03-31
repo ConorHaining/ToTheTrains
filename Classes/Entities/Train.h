c757f7d2172f75976e971c813227dba07041be57
Conor Haining <conor.haining@gmail.com>
2018-03-31 18:28:56 +0100

    New Train Entity

---
+++ Classes/Entities/Train.h
@@ -0,0 +1,14 @@
+//
+// Created by conor on 31/03/18.
+//
+
+#ifndef PROJ_ANDROID_STUDIO_TRAIN_H
+#define PROJ_ANDROID_STUDIO_TRAIN_H
+
+
+class Train {
+
+};
+
+
+#endif //PROJ_ANDROID_STUDIO_TRAIN_H
--- proj.android-studio/app/jni/Android.mk
+++ proj.android-studio/app/jni/Android.mk
@@ -29,6 +29,7 @@ LOCAL_SRC_FILES := $(LOCAL_PATH)/hellocpp/main.cpp \
                    $(LOCAL_PATH)/../../../Classes/Components/Time.cpp \
                    $(LOCAL_PATH)/../../../Classes/Components/SpriteComponent.cpp \
                    $(LOCAL_PATH)/../../../Classes/Entities/Station.cpp \
+                   $(LOCAL_PATH)/../../../Classes/Entities/Train.cpp \
                    $(LOCAL_PATH)/../../../Classes/Systems/StationRenderSystem.cpp
 
 
