7c40c7314fb3e5cf5db58fbf7d1bf05eb553a755
Conor Haining <conor.haining@gmail.com>
2018-04-03 15:34:08 +0100

    New Label Component

---
+++ Classes/Components/LabelComponent.cpp
@@ -0,0 +1,5 @@
+//
+// Created by conor on 03/04/18.
+//
+
+#include "LabelComponent.h"
---
+++ Classes/Components/LabelComponent.h
@@ -0,0 +1,16 @@
+//
+// Created by conor on 03/04/18.
+//
+
+#ifndef PROJ_ANDROID_STUDIO_LABELCOMPONENT_H
+#define PROJ_ANDROID_STUDIO_LABELCOMPONENT_H
+
+
+#include "../ComponentInterface.h"
+
+class LabelComponent : public ComponentInterface{
+
+};
+
+
+#endif //PROJ_ANDROID_STUDIO_LABELCOMPONENT_H
