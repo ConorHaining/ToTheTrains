7db808f11eba8c5eb6b0a17cecc11e13832081ba
Conor Haining <conor.haining@gmail.com>
2018-05-03 15:38:38 +0100

    Builds and Spawns Train

--- Classes/Systems/TrainSpawnSystem.h
+++ Classes/Systems/TrainSpawnSystem.h
@@ -5,9 +5,19 @@
 #ifndef PROJ_ANDROID_STUDIO_TRAINSPAWNSYSTEM_H
 #define PROJ_ANDROID_STUDIO_TRAINSPAWNSYSTEM_H
 
+#include <vector>
+#include <Entities/Train.h>
+#include <include/rapidjson/document.h>
+
+using namespace std;
 
 class TrainSpawnSystem {
-
+private:
+    vector<Train> activeTrains;
+    cocos2d::Scene *scene;
+public:
+    TrainSpawnSystem(cocos2d::Scene *scene);
+    void spawnTrain(rapidjson::Value& timetableRecord);
 };
 
 
