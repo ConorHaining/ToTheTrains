f69282279f735b4e568b02b6b3f8d1825df245dc
Conor Haining <conor.haining@gmail.com>
2018-05-11 15:59:23 +0100

    New Score Entity

---
+++ Classes/Entities/ScoreEntity.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Conor Haining on 11/05/2018.
+//
+
+#include "ScoreEntity.h"
---
+++ Classes/Entities/ScoreEntity.h
@@ -0,0 +1,16 @@
+//
+// Created by Conor Haining on 11/05/2018.
+//
+
+#ifndef PROJ_ANDROID_STUDIO_SCOREENTITY_H
+#define PROJ_ANDROID_STUDIO_SCOREENTITY_H
+
+
+#include "../EntityInterface.h"
+
+class ScoreEntity : public EntityInterface {
+
+};
+
+
+#endif //PROJ_ANDROID_STUDIO_SCOREENTITY_H
